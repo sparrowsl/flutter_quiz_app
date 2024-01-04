63199b14283f59507bb9b298655be0c274e2356e
sparrowsl <benthorpe.dev@gmail.com>
2024-01-04 17:46:51 +0000

    refactor: add icon to start button

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -5,29 +5,29 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset("assets/images/quiz.png"),
-            const SizedBox(height: 10),
-            const Text(
-              "Learn Flutter the cool way!!",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.purple,
-                fontWeight: FontWeight.bold,
-              ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset("assets/images/quiz.png", color: Colors.blueAccent),
+          const SizedBox(height: 10),
+          const Text(
+            "Learn Flutter the cool way!!",
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Start Quiz", style: TextStyle(fontSize: 16)),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton.icon(
+            onPressed: () {},
+            label: const Text("Start Quiz",
+                style: TextStyle(fontSize: 16, color: Colors.blueAccent)),
+            icon: const Icon(Icons.arrow_right_alt),
+          )
+        ],
       ),
     );
   }
